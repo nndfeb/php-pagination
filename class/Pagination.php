63f278759ea49a9da7a593f0177032af8fadaa8e
nendifebrianto1 <nendifebrianto1@gmail.com>
2020-04-06 17:13:48 +0700

    commit kedua

--- class/Pagination.php
+++ class/Pagination.php
@@ -16,13 +16,48 @@ class Pagination
 
     function set_total_records()
     {
-        $stmt = $this->db->prepare("SELECT id FROM $this->table");
+        $stmt = $this->db->prepare("SELECT username FROM $this->table");
         $stmt->execute();
         $this->total_records = $stmt->rowCount();
     }
     public function get_data()
     {
         $start = 0;
+        if ($this->current_page() > 1) {
+            $start = ($this->current_page() * $this->limit) - $this->limit;
+        }
+
         $stmt = $this->db->prepare("SELECT * FROM $this->table LIMIT $start, $this->limit");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function current_page()
+    {
+        // default page number 1
+        return isset($_GET['page']) ? (int) $_GET['page'] : 1;
+    }
+
+    public function get_pagination_numbers()
+    {
+        // ceil untuk pembulatan angka
+        return ceil($this->total_records / $this->limit);
+
+        // 20/5
+        // 4
+        // 1 2 3 4 
+    }
+    public function prev_page()
+    {
+        return ($this->current_page() > 1) ? $this->current_page() - 1 : 1;
+    }
+    public function next_page()
+    {
+        return ($this->current_page() < $this->get_pagination_numbers()) ? $this->current_page() + 1 : $this->get_pagination_numbers();
+    }
+
+    public function is_active_page($num)
+    {
+        return ($num == $this->current_page()) ? 'active' : '';
     }
 }
--- index.php
+++ index.php
@@ -4,5 +4,43 @@ require_once 'class/Pagination.php';
 
 $pagination = new Pagination('users');
 
-
 $users = $pagination->get_data();
+$pages = $pagination->get_pagination_numbers();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagination PDO Class</title>
+    <style>
+        .active {
+            background-color: rgb(23, 169, 201);
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($users as $user) : ?>
+            <li>
+                <?= $user->username . ':' . $user->email ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+    <hr>
+
+    <a href="?page=<?= $pagination->prev_page() ?>">
+        << </a> <?php for ($i = 1; $i <= $pages; $i++) : ?> <a class="<?= $pagination->is_active_page($i); ?>" href="?page=<?= $i ?> "> <?= $i ?>
+    </a>
+<?php endfor; ?>
+<a href="?page=<?= $pagination->next_page() ?>">
+    >> </a>
+</body>
+
+</html>
